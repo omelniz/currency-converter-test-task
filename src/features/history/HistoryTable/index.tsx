e471f10193b36bd46b6fb5e5361ae3ff97651f1f
Aleksandr Omelnitckii <omelniz@gmail.com>
2020-07-12 11:24:30 +0300

    Implement history table component

---
+++ src/features/history/HistoryTable/index.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import Table, { Thead, Tbody } from "./../../../components/Table";
+
+interface IHistoryRecord {
+  asset: string;
+  startDate: string;
+  startQuote: string;
+  finishDate: string;
+  finishQuote: string;
+  profit: string;
+}
+
+export interface IHistoryTableProps {
+  items: Array<IHistoryRecord>;
+}
+
+const HistoryTable: React.FC<IHistoryTableProps> = ({ items }) => (
+  <div data-testid="history">
+    <Table>
+      <Thead>
+        <tr>
+          <th>Актив</th>
+          <th>Начало</th>
+          <th>Котировка</th>
+          <th>Конец</th>
+          <th>Котировка</th>
+          <th>Прибыль</th>
+        </tr>
+      </Thead>
+      <Tbody>
+        {items.map((item) => (
+          <tr key={item.asset}>
+            <td>{item.asset}</td>
+            <td>{item.startDate}</td>
+            <td>{item.startQuote}</td>
+            <td>{item.finishDate}</td>
+            <td>{item.finishQuote}</td>
+            <td>{item.profit}</td>
+          </tr>
+        ))}
+      </Tbody>
+    </Table>
+  </div>
+);
+
+HistoryTable.defaultProps = {
+  items: [],
+};
+
+export default HistoryTable;
